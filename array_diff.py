07d6795f516acecc13e312cd4c8b258180533fc6
lawrence-ms <67106432+lawrence-ms@users.noreply.github.com>
2021-06-10 21:47:38 -0300

    Delete array_diff.py

--- array_diff.py
+++
@@ -1,9 +0,0 @@
-# https://www.codewars.com/kata/523f5d21c841566fde000009
-
-def array_diff(a, b):
-    new_list = []
-    for char in a:
-        if char not in b:
-            new_list.append(char)
-    return new_list
-  
